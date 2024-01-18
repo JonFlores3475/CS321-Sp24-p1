3c2fe4e8ba4d6a1aedc658ac88962145b4315092
Jon Flores <jonathanflores@u.boisestate.edu>
2024-01-18 11:56:16 -0700

    CacheTest is now written. Actually testing required.

--- Cache/src/Cache.java
+++ Cache/src/Cache.java
@@ -1,31 +1,31 @@
 import java.util.LinkedList;
 import java.util.ListIterator;
-public class Cache <E> {
-    private double r1 = 0;
-    private double r2 = 0;
-    private double HC1 = 0;
-    private double HC2 = 0;
-    private int cache1_size;
-    private int cache2_size;
-    private E temp;
-    private LinkedList<E> Cache1;
-    private LinkedList<E> Cache2;
+public class Cache {
+    private static double r1 = 0;
+    private static double r2 = 0;
+    private static double HC1 = 0;
+    private static double HC2 = 0;
+    private static int cache1_size;
+    private static int cache2_size;
+    private static Object temp;
+    private static LinkedList<Object> Cache1;
+    private static LinkedList<Object> Cache2;
 
-    public Cache(int args1, int args2) {
+    public Cache(int args1, int args2, String args3) {
         cache1_size = args1;
         cache2_size = args2;
         Cache1 = new LinkedList<>();
         Cache2 = new LinkedList<>();
     }
 
-    public E get(E key) {
+    public static Object get(Object key) {
         r1++;
         if (Cache1.isEmpty() && Cache2.isEmpty() || !Cache1.contains(key) && !Cache2.contains(key)) {
             return null;
         }
         if (!Cache1.contains(key) && Cache2.contains(key)) {
             r2++;
-            ListIterator<E> I = Cache2.listIterator(0);
+            ListIterator<?> I = Cache2.listIterator(0);
             while (I.hasNext()) {
                 if (I.next().equals(key)) {
                     HC2++;
@@ -39,7 +39,7 @@ public class Cache <E> {
         }
         if (Cache1.contains(key) && !Cache2.contains(key)) {
             Cache2.addFirst(key);
-            ListIterator<E> J = Cache1.listIterator(0);
+            ListIterator<?> J = Cache1.listIterator(0);
             while (J.hasNext()) {
                 if (J.next().equals(key)) {
                     HC1++;
@@ -53,7 +53,7 @@ public class Cache <E> {
         return null;
     }
 
-    public E add(E value) {
+    public static Object add(Object value) {
         if (Cache1.size() < cache1_size && Cache2.size() < cache2_size) {
             Cache1.addFirst(value);
             Cache2.addFirst(value);
@@ -81,12 +81,12 @@ public class Cache <E> {
         }
         return null;
     }
-    public E remove(E key){
+    public Object remove(Object key){
         if(Cache1.isEmpty() && Cache2.isEmpty() || !Cache1.contains(key) && !Cache2.contains(key)){
             return null;
         }
         if(Cache1.contains(key)){
-            ListIterator<E> K = Cache1.listIterator(0);
+            ListIterator<Object> K = Cache1.listIterator(0);
             while(K.hasNext()){
                 if(K.next().equals(key)){
                     temp = Cache1.get(K.previousIndex());
@@ -96,7 +96,7 @@ public class Cache <E> {
             }
         }
         if(Cache2.contains(key)){
-            ListIterator <E> L = Cache2.listIterator(0);
+            ListIterator<Object> L = Cache2.listIterator(0);
             while(L.hasNext()){
                 if(L.next().equals(key)){
                     temp = Cache2.get(L.previousIndex());
---
+++ Cache/src/CacheTest.java
@@ -0,0 +1,61 @@
+import static java.lang.Integer.parseInt;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileWriter;
+
+
+public class CacheTest extends Cache {
+    private static final long start = System.currentTimeMillis();
+    private final Cache Cache;
+
+    public CacheTest(int args1, int args2, String args3) {
+        super(args1, args2, args3);
+        Cache = new Cache(args1, args2, args3);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        if (!(args.length == 3)) {
+            System.out.println("\nUsage: java CacheTest <cache1-size> <cache2-size> <input file name>");
+            System.exit(1);
+        }
+        CacheTest T = new CacheTest(parseInt(args[0]), parseInt(args[1]), args[2]);
+        File file = new File(args[2]);
+        Scanner scan = new Scanner(file);
+        while (scan.hasNext()) {
+            String key = scan.next();
+            if (get(key) == null) {
+                add(key);
+            }
+        }
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.println(T.Cache.toString());
+        try {
+            File out = new File("README.txt");
+            if (out.createNewFile()) {
+                System.out.println("\nFile successfully created: " + out.getName());
+            } else {
+                System.out.println("\nFile already exists.");
+                out.delete();
+                System.out.println("\nFile deleted.");
+                out = new File("README.txt");
+                System.out.println("\nFile recreated.");
+            }
+        } catch (IOException e) {
+            System.out.println("\nAn error occurred.");
+            e.printStackTrace();
+        }
+        try{
+            FileWriter writer = new FileWriter("README.txt");
+            writer.write(elapsed+" MS");
+            writer.close();
+            System.out.println("\nSuccessfully wrote to file.");
+        }
+        catch(IOException e){
+            System.out.println("\nAn error occurred.");
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+}

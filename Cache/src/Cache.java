db273581780c2cbebcab52dd19e9da51d4ed0e5f
Jon Flores <jonathanflores@u.boisestate.edu>
2024-01-23 05:54:38 -0700

    All functionality as Cache and 2Cache. Cache hits still off though.

--- Cache/src/Cache.java
+++ Cache/src/Cache.java
@@ -5,19 +5,43 @@ public class Cache {
     private static int r2 = 0;
     private static double HC1 = 0;
     private static double HC2 = 0;
-    private static boolean detect = false;
     private static int cache1_size;
     private static int cache2_size;
+    private static boolean detect = false;
+    private static Object temp;
     private static LinkedList<Object> Cache1;
     private static LinkedList<Object> Cache2;
 
-    public Cache(int args1, int args2, String args3) {
-        cache1_size = args1;
-        cache2_size = args2;
-        Cache1 = new LinkedList<>();
-        Cache2 = new LinkedList<>();
+    public Cache(int args0, int args1, int args2, String args3) {
+        if(args0 == 2) {
+            cache1_size = args1;
+            cache2_size = args2;
+            Cache1 = new LinkedList<>();
+            Cache2 = new LinkedList<>();
+        }
+        if(args0 == 1){
+            cache1_size = args1;
+            Cache1 = new LinkedList<>();
+        }
     }
-    public static Object get(Object key) {
+    public static Object get1(Object key){
+        r1++;
+        if(Cache1.isEmpty()){
+            add1(key);
+            return key;
+        }
+        ListIterator<Object> I = Cache1.listIterator(0);
+        while (I.hasNext()) {
+            if (I.next().equals(key)) {
+                HC1++;
+                I.remove();
+                break;
+            }
+        }
+        add1(key);
+        return key;
+    }
+    public static Object get2(Object key) {
         r1++;
         if (Cache1.isEmpty() && Cache2.isEmpty()) {
             r1++;
@@ -27,82 +51,165 @@ public class Cache {
             r2++;
         }
         ListIterator<Object> I = Cache1.listIterator(0);
-        ListIterator<Object> J = Cache2.listIterator(0);
         while (I.hasNext()) {
             if (I.next().equals(key)) {
                 HC1++;
                 I.remove();
                 detect = true;
+                break;
             }
         }
         if(!detect) {
+            ListIterator<Object> J = Cache2.listIterator(0);
             r2++;
             while (J.hasNext()) {
                 if (J.next().equals(key)) {
                     HC2++;
                     J.remove();
+                    break;
                 }
             }
         }
-        add(key);
+        add2(key);
         detect = false;
         return key;
     }
+    public static Object add1(Object value){
+        if(Cache1.isEmpty()) {
+            Cache1.addFirst(value);
+            return value;
+        }
+        if(Cache1.size() >= cache1_size){
+            while(Cache1.size() >= cache1_size){
+                Cache1.removeLast();
+            }
+        }
+        Cache1.addFirst(value);
+        return value;
+    }
 
-    public static Object add(Object value) {
+    public static Object add2(Object value) {
+        if(!Cache1.isEmpty() && Cache2.isEmpty()){
+            Cache2 = Cache1;
+            Cache1.addFirst(value);
+            Cache2.addFirst(value);
+            return value;
+        }
+        if(Cache1.isEmpty() && Cache2.size() < cache2_size) {
+            Cache1.addFirst(value);
+            Cache2.addFirst(value);
+            return value;
+        }
+        if(Cache1.isEmpty()){
+            while(Cache2.size() >= cache2_size){
+                Cache2.removeLast();
+            }
+            Cache1 = Cache2;
+            while(Cache1.size() >= cache1_size){
+                Cache1.removeLast();
+            }
+            Cache1.addFirst(value);
+            Cache2.addFirst(value);
+            return value;
+        }
         if (Cache1.size() < cache1_size && Cache2.size() < cache2_size) {
             Cache1.addFirst(value);
             Cache2.addFirst(value);
             return value;
         }
-        if (Cache1.size() == cache1_size && Cache2.size() < cache2_size) {
-            Cache2.addFirst(value);
-            Cache1.removeLast();
-            Cache1.addFirst(value);
-            return value;
-        }
-        if (Cache2.size() == cache2_size && Cache1.size() < cache1_size) {
+
+        if (Cache1.size() < cache1_size && Cache2.size() == cache2_size) {
             Cache2.removeLast();
             Cache1.addFirst(value);
             Cache2.addFirst(value);
             return value;
         }
-        if(Cache2.size() == cache2_size && Cache1.size() == cache1_size){
+        if(Cache1.size() == cache1_size && Cache2.size() == cache2_size){
+            temp = Cache1.getLast();
             Cache1.removeLast();
+            Cache2.removeLast();
+            Cache2.addFirst(temp);
             Cache2.removeLast();
             Cache1.addFirst(value);
             Cache2.addFirst(value);
             return value;
         }
-        if(Cache2.size() > cache2_size){
+        if(Cache1.size() == cache1_size && Cache2.size() < cache2_size){
+            temp = Cache1.getLast();
+            Cache1.removeLast();
+            Cache2.addFirst(temp);
+            if(Cache2.size() == cache2_size){
+                Cache2.removeLast();
+            }
+            Cache1.addFirst(value);
+            Cache2.addFirst(value);
+            return value;
+        }
+        if(Cache1.size() > cache1_size && Cache2.size() == cache2_size){
+            while(Cache1.size() >= cache1_size){
+                temp = Cache1.getLast();
+                Cache2.removeLast();
+                Cache2.addFirst(temp);
+            }
+            Cache1.addFirst(value);
+            Cache2.removeLast();
+            Cache2.addFirst(value);
+            return value;
+        }
+        if(Cache1.size() > cache1_size && Cache2.size() > cache2_size){
+            while(Cache1.size() >= cache1_size){
+                temp = Cache1.getLast();
+                Cache2.removeLast();
+                Cache2.addFirst(temp);
+                Cache1.removeLast();
+            }
+            while(Cache2.size() >= cache2_size){
+                Cache2.removeLast();
+            }
+            Cache1.addFirst(value);
+            Cache2.addFirst(value);
+            return value;
+        }
+        if(Cache1.size() < cache1_size){
             while(Cache2.size() >= cache2_size){
                 Cache2.removeLast();
             }
             Cache2.addFirst(value);
+            Cache1.addFirst(value);
+            return value;
         }
         if(Cache1.size() > cache1_size){
             while(Cache1.size() >= cache1_size){
+                temp = Cache1.getLast();
+                Cache2.addFirst(temp);
                 Cache1.removeLast();
             }
-            Cache1.addFirst(value);
-        }
-        if(Cache1.size() > cache1_size && Cache2.size() > cache2_size){
-            while(Cache2.size() >= cache2_size){
-                Cache2.removeLast();
+            if(Cache2.size() >= cache2_size){
+                add2(value);
             }
+            Cache1.addFirst(value);
             Cache2.addFirst(value);
-            while(Cache1.size() >= cache1_size){
-                Cache1.removeLast();
-            }
-            Cache1.addFirst(value);
+            return value;
         }
         return value;
     }
-    public Object remove(Object key){
+    public Object remove1(Object key){
+        if(Cache1.contains(key)){
+            ListIterator<Object> K = Cache1.listIterator(0);
+            while(K.hasNext()){
+                if(K.next().equals(key)){
+                    temp = Cache1.get(K.previousIndex());
+                    K.remove();
+                    return temp;
+                }
+            }
+        }
+        return null;
+    }
+    public Object remove2(Object key){
         if(Cache1.isEmpty() && Cache2.isEmpty() || !Cache1.contains(key) && !Cache2.contains(key)){
             return null;
         }
-        Object temp;
         if(Cache1.contains(key)){
             ListIterator<Object> K = Cache1.listIterator(0);
             while(K.hasNext()){
@@ -125,14 +232,18 @@ public class Cache {
         }
         return null;
     }
-    public void clear(){
-        if(!Cache1.isEmpty() && !Cache2.isEmpty()){
-            Cache1.clear();
-            Cache2.clear();
-        }
+    public static void clear1(){
+        Cache1.clear();
     }
-    @Override
-    public String toString(){
+    public static void clear2(){
+        Cache1.clear();
+        Cache2.clear();
+    }
+    public String toString1(){
+        double HR1 = (HC1 / r1);
+        return("First level cache with " + cache1_size + " entries has been created\n..............................\nThe number of global references:\t" + r1 + "\nThe number of global cache hits:\t" + HC1 +"\nThe global hit ratio\t\t\t:\t" + HR1 + "\n\nThe number of 1st-level references:\t" + r1 + "\nThe number of 1st-level cache hits:\t" + HC1);
+    }
+    public String toString2(){
         double HC = HC1 + HC2;
         double HR1 = (HC1 / r1);
         double HR2 = (HC2 / r2);
--- Cache/src/Test.java
+++ Cache/src/Test.java
@@ -9,26 +9,47 @@ import java.io.FileWriter;
 public class Test extends Cache {
     private static final long start = System.currentTimeMillis();
     private final Cache Cache;
+    private static Test T;
+    private static File file;
 
-    public Test(int args1, int args2, String args3) {
-        super(args1, args2, args3);
-        Cache = new Cache(args1, args2, args3);
+    public Test(int args0, int args1, int args2, String args3) {
+        super(args0, args1, args2, args3);
+        Cache = new Cache(args0, args1, args2, args3);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        if (!(args.length == 3)) {
-            System.out.println("\nUsage: java Test <cache1-size> <cache2-size> <input file name>");
+        if ((args.length < 3 || args.length > 4) || (parseInt(args[0]) < 1 || parseInt(args[0]) > 2)){
+            System.out.println("\nUsage: java Test <1> <cache1-size> <input file name>\nfor single cache or\n java Test <2> <cache1-size> <cache2-size> <input file name>\nfor 2Cache.");
             System.exit(1);
         }
-        Test T = new Test(parseInt(args[0]), parseInt(args[1]), args[2]);
-        File file = new File(args[2]);
+        if(args.length == 3){
+            T = new Test(parseInt(args[0]), parseInt(args[1]), 0, args[2]);
+            file = new File(args[2]);
+        }
+        if(args.length == 4) {
+            T = new Test(parseInt(args[0]), parseInt(args[1]), parseInt(args[2]), args[3]);
+            file = new File(args[3]);
+        }
         Scanner scan = new Scanner(file);
-        while (scan.hasNext()) {
-            String key = scan.next();
-            get(key);
+        if(parseInt(args[0]) == 2) {
+            while (scan.hasNext()) {
+                String key = scan.next();
+                get2(key);
+            }
+        }
+        if(parseInt(args[0]) == 1){
+            while (scan.hasNext()) {
+                String key = scan.next();
+                get1(key);
+            }
         }
         long elapsed = System.currentTimeMillis() - start;
-        System.out.println(T.Cache.toString());
+        if(parseInt(args[0]) == 2) {
+            System.out.println(T.Cache.toString2());
+        }
+        if(parseInt(args[0]) == 1){
+            System.out.println(T.Cache.toString1());
+        }
         try {
             File out = new File("README.txt");
             if (out.createNewFile()) {
